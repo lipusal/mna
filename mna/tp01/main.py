6fe1ec4e8f1619033917ff654c0030022f00d7a8
Juan Li Puma <juanlipuma94@gmail.com>
2017-09-03 22:53:02 -0300

    Add requirements, basic main
    - Add Pillow dependency
    - Implement basic main to read and get image bytes

---
+++ mna/tp01/main.py
@@ -0,0 +1,21 @@
+from os import path
+import argparse
+from PIL import Image
+
+parser = argparse.ArgumentParser(description="Face recognizer. Receives an image and identifies it in a database.")
+parser.add_argument("images", nargs="?", help="Images to recognize", type=str)
+parser.add_argument("--verbose", "-v", help="Print verbose information while running", action="store_true", default=False)
+args = parser.parse_args()
+
+if args.images is None:
+    print("At least 1 image is required. Aborting.")
+    exit(1)
+elif type(args.images) is not list:
+    args.images = [args.images]
+
+images = []
+for raw_path in args.images:
+    # For each image, save an array of (R,G,B) tuples, one per pixel
+    images.append(list(Image.open(path.normpath(raw_path)).getdata()))
+
+print("Read bytes of %i images" % len(images))
