5100922d0391d3e0fbecc6466d064f5f9fc06789
Juan Li Puma <juan.lipuma@restorando.com>
2017-09-10 17:31:48 -0300

    Normalize images, calculate eigenvectors

--- mna/tp01/main.py
+++ mna/tp01/main.py
@@ -1,9 +1,10 @@
 from os import path
 import argparse
 from PIL import Image
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Face recognizer. Receives an image and identifies it in a database.")
-parser.add_argument("images", nargs="?", help="Images to recognize", type=str)
+parser.add_argument("images", nargs="+", help="Images to recognize", type=str)
 parser.add_argument("--verbose", "-v", help="Print verbose information while running", action="store_true",
                     default=False)
 args = parser.parse_args()
@@ -23,3 +24,23 @@ for raw_path in args.images:
             .getdata()))
 
 print("Read bytes of %i images" % len(images))
+
+# TODO: Verify all images have the same size
+
+# Normalize data
+average_pixels = np.mean(images, 0)     # Average value for each pixel
+# TODO: Dividir por varianza?
+
+for i in range(len(images)):
+    for j in range(len(images[i])):
+        images[i][j] -= average_pixels[j]
+
+# Calculate covariance matrix
+# cov = np.cov(images, rowvar=True)
+# eigenvalues, eigenvectors = np.linalg.eig(cov)
+
+_, __, eigenvectors = np.linalg.svd(images, full_matrices=False)
+
+
+
+print(len(eigenvectors))
