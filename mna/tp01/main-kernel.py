b5d05050c69dc13bea9f6e131734556dba716482
Juan Li Puma <juanlipuma94@gmail.com>
2017-09-18 23:31:06 -0300

    Add KPCA main

---
+++ mna/tp01/main-kernel.py
@@ -0,0 +1,136 @@
+import argparse
+import numpy as np
+from mna.tp01.utils.images import *
+# import matplotlib.pyplot as plt
+from sklearn import svm
+
+parser = argparse.ArgumentParser(description="Face recognizer. Receives an image and identifies it in a database.")
+parser.add_argument("directory", help="Directory from which to load images. Directory should have further "
+                                      "subdirectories, where each subdirectory belongs to a different individual. Each "
+                                      "individual should have the same amount of pictures.", type=str)
+parser.add_argument("num_train", help="Number of pictures per individual to take as training pictures.", type=int)
+parser.add_argument("num_test", help="Number of pictures per individual to take as testing pictures.", type=int)
+parser.add_argument("--verbose", "-v", help="Print verbose information while running", action="store_true",
+                    default=False)
+parser.add_argument("--cutoff", "-c", help="Percentage of captured variance at which to cut off using eigenfaces. "
+                                           "Decimal in (0, 1]. Default is 0.9", type=float, default=0.9)
+parser.add_argument("--time", "-t", help="Print elapsed program time", action="store_true", default=False)
+args = parser.parse_args()
+
+if args.time:
+    import mna.tp01.utils.timer
+
+# Open images and separate them in training and testing groups
+if args.verbose:
+    print("Loading pictures...")
+
+num_individuals, train_images, test_images = open_images(args)
+num_train_pics = num_individuals * args.num_train
+num_test_pics = num_individuals * args.num_test
+
+
+# Normalize training images
+if args.verbose:
+    print("Normalizing pictures...")
+
+# Make numbers easier to play with
+# TODO: Define whether ** 2 stays or not
+train_images = (np.asarray(train_images) - 127.5) / 127.5
+test_images = (np.asarray(test_images) - 127.5) / 127.5
+
+# TODO: El código de referencia no hace esto, ver por qué no centramos las fotos así en vez de con lo de abajo
+# # Subtract mean from train images
+# mean_face = mean_image(train_images)
+# train_images -= mean_face
+# # Also center testing images with respect to training images
+# test_images -= mean_face
+
+if args.verbose:
+    print("Computing kernel matrices...")
+
+# Define and compute testing and training kernels
+# Both kernels are polynomial of degree `degree`
+degree = 2
+
+train_kernel = ((np.dot(train_images, train_images.T)/num_train_pics) + 1) ** degree
+# Center kernel in the high-dimension space with the following trick (ver filmina 27 de teórica Kernel Trick)
+ones_train = np.ones([num_train_pics, num_train_pics]) / num_train_pics
+train_kernel = train_kernel - np.dot(ones_train, train_kernel) - np.dot(train_kernel, ones_train) + np.dot(ones_train, np.dot(train_kernel, ones_train))
+
+test_kernel = ((np.dot(test_images, train_images.T)/num_train_pics) + 1) ** degree
+# Also center test kernel
+ones_test = np.ones([num_test_pics, num_train_pics]) / num_train_pics
+test_kernel = test_kernel - np.dot(ones_test, train_kernel) - np.dot(test_kernel, ones_train) + np.dot(ones_test, np.dot(train_kernel, ones_train))
+
+
+if args.verbose:
+    print("Finding eigenfaces...")
+
+# TODO: Use our functions for this
+eigenvalues, eigenfaces = np.linalg.eigh(train_kernel)
+# Eigenvalues/vectors are returned in ascending order, flip to descending
+eigenvalues = np.flipud(eigenvalues)
+eigenfaces = np.fliplr(eigenfaces)
+
+# Normalize eigenfaces (i.e. divide by their norm)
+for i in range(len(eigenfaces)):
+    eigenfaces[:, i] = eigenfaces[:, i] / np.sqrt(eigenvalues[i])
+
+# Get enough eigenvalues to capture at least the specified variance
+cumulative_sum = 0
+eigenvalues_sum = sum(eigenvalues)
+
+used_eigenfaces = 0
+for i in range(len(eigenfaces)):
+    used_eigenfaces += 1
+    cumulative_sum += eigenvalues[i]
+    if cumulative_sum/eigenvalues_sum >= args.cutoff:
+        break
+
+if args.verbose:
+    if cumulative_sum / eigenvalues_sum < args.cutoff:
+        print("[WARN]: Couldn't capture desired variance (%g) with all %i eigenfaces (captured %g), continuing"
+              % (args.cutoff, len(eigenfaces), cumulative_sum / eigenvalues_sum))
+    else:
+        print("Captured desired variance (%g) with %i/%i eigenfaces" % (args.cutoff, used_eigenfaces, len(eigenfaces)))
+
+# Project each image to all chosen eigenfaces
+if args.verbose:
+    print("Projecting images to eigenfaces...")
+
+projected_train_imgs = np.dot(train_kernel.T, eigenfaces)
+projected_test_imgs = np.dot(test_kernel, eigenfaces)
+
+# Show mean face
+# fig, axes = plt.subplots(1,1)
+# axes.imshow(np.reshape(mean_pixels,[112,92]),cmap='gray')
+# fig.suptitle('Mean face')
+# fig.show()
+
+# Show all eigenfaces
+# for eigenface in eigenfaces:
+#     fig, axes = plt.subplots(1, 1)
+#     axes.imshow(np.reshape(eigenface, [112, 92]), cmap='gray')
+#     fig.suptitle('Autocara')
+#     fig.show()
+
+
+clf = svm.LinearSVC()
+
+# Get which pictures belong to which individual
+train_classes = list()
+test_classes = list()
+for i in range(num_individuals):
+    train_classes += [i] * args.num_train
+    test_classes += [i] * args.num_test
+
+if args.verbose:
+    print("Training and testing picture categories...")
+
+# Truncate number of used eigenfaces
+projected_train_imgs = projected_train_imgs[:, 0:used_eigenfaces]
+projected_test_imgs = projected_test_imgs[:, 0:used_eigenfaces]
+
+clf.fit(projected_train_imgs, train_classes)
+classifications = clf.score(projected_test_imgs, test_classes)
+print("Classification accuracy with %i eigenfaces: %g%%" % (used_eigenfaces, classifications*100))
